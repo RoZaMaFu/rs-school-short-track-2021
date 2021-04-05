b0c125f1f190c7792cb23fad963334dc6d9761f3
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-05 23:40:53 +0300

    feat: add common letters func

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,24 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
-}
+function getCommonCharacterCount(s1, s2) {
+        let s1Arr = s1.split('')
+        let s2Arr = s2.split('')
+        let commonLetters = 0
+        s1Arr.map(s1Letter => {
+            s2Arr.map(s2Letter => {
+                if(s1Letter === s2Letter){
+                    s1Arr[s1Arr.indexOf(s1Letter)] = 0
+                    s2Arr[s2Arr.indexOf(s2Letter)] = 0
+                }
+            })
+        })
+        s1Arr.map(letter => {
+            if(letter === 0){
+                commonLetters++
+            }
+        })
+        return commonLetters
+    }
 
 module.exports = getCommonCharacterCount;
