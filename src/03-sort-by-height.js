709768018bf591bb21cb46cba11b7d0d9ca0b6f3
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 19:40:04 +0300

    fix: remove spaces

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -1,14 +1,3 @@
-/**
- * Given an array with heights, sort them except if the value is -1.
- *
- * @param {Array} arr
- * @return {Array}
- *
- * @example
- * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
- *
- * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
- */
 function sortByHeight(arr) {
     let tempArr = arr.filter(numb => numb !== -1).sort()
     let increment = 0
