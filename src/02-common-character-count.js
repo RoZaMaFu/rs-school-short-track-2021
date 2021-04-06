bfc897af5329f13fab928c1ad151a78f41390b08
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 19:39:29 +0300

    fix: remove spaces

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -1,14 +1,3 @@
-/**
- * Given two strings, find the number of common characters between them.
- *
- * @param {String} s1
- * @param {String} s2
- * @return {Number}
- *
- * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
- * Strings have 3 common characters - 2 "a"s and 1 "c".
- */
 function getCommonCharacterCount(s1, s2) {
         let s1Arr = s1.split('')
         let s2Arr = s2.split('')
