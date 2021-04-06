703c1c13882fe7d3af29748015839304234be960
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 20:46:45 +0300

    upd: eslint fixes

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -1,17 +1,24 @@
 function getDNSStats(domains) {
-    let appearanceObj = {}
-    domains.map(domain => {
-        let toDNS = []
-        domain.split('.').map(dns => {
-            toDNS.unshift(dns)
-        })
-        let strDNS = ''
-        toDNS.map(dns => {
-            strDNS += `.${dns}`
-            appearanceObj.hasOwnProperty(strDNS) ? appearanceObj[strDNS]++ : appearanceObj[strDNS] = 1
-        })
-    })
-    return appearanceObj
+  const appearanceObj = {};
+  domains.map((domain) => {
+    const toDNS = [];
+    domain.split('.').map((dns) => {
+      toDNS.unshift(dns);
+      return dns;
+    });
+    let strDNS = '';
+    toDNS.map((dns) => {
+      strDNS += `.${dns}`;
+      if (appearanceObj[strDNS]) {
+        appearanceObj[strDNS]++;
+      } else {
+        appearanceObj[strDNS] = 1;
+      }
+      return dns;
+    });
+    return domain;
+  });
+  return appearanceObj;
 }
 
 module.exports = getDNSStats;
