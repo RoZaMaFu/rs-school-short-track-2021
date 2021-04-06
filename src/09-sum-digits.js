88ce6e4062f97a4a725136aa6775102e54daff44
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 20:45:17 +0300

    upd: eslint fixes

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -1,9 +1,10 @@
 function getSumOfDigits(n) {
-    let sum = 0
-    n.toString().split('').map(digit => {
-        sum += Number(digit)
-    })
-    return sum.toString().length === 1 ? sum : getSumOfDigits(sum)
+  let sum = 0;
+  n.toString().split('').map((digit) => {
+    sum += Number(digit);
+    return digit;
+  });
+  return sum.toString().length === 1 ? sum : getSumOfDigits(sum);
 }
 
 module.exports = getSumOfDigits;
