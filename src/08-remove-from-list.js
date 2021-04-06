f8dc6f624bcf1ea1d65c89ca0bbcb6af4a79c561
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 14:26:41 +0300

    feat: add remove func

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,13 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
-}
+function removeKFromList(l, k) {
+    l.map((int, i) => {
+        if(int  === k ){
+            l.splice(i, 1)
+        }
+    })
+    return l
+  }
 
 module.exports = removeKFromList;
