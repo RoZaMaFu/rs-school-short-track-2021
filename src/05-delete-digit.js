7beac997f07e9dbc8e1cd0685caddd05ecb612da
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 19:40:52 +0300

    fix: remove spaces

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -1,14 +1,3 @@
-/**
- * Given some integer, find the maximal number you can obtain
- * by deleting exactly one digit of the given number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For n = 152, the output should be 52
- *
- */
 function deleteDigit(n) {
         let maxNum = 0
         let numArr = n.toString().split('')
