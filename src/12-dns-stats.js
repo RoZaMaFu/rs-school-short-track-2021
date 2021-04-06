4e501168b748bfcdd6758098e3c046b9dc402a01
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 15:30:16 +0300

    feat: add dns convert func

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,20 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+    let appearanceObj = {}
+    domains.map(domain => {
+        let toDNS = []
+        domain.split('.').map(dns => {
+            toDNS.unshift(dns)
+        })
+        let strDNS = ''
+        toDNS.map(dns => {
+            strDNS += `.${dns}`
+            appearanceObj.hasOwnProperty(strDNS) ? appearanceObj[strDNS]++ : appearanceObj[strDNS] = 1
+        })
+    })
+    return appearanceObj
 }
 
 module.exports = getDNSStats;
