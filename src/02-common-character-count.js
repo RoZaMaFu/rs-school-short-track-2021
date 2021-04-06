8bf23274e192c61ff39fd075b54a244ee5d2a7d0
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 20:41:43 +0300

    upd: eslint fixes

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -1,21 +1,24 @@
 function getCommonCharacterCount(s1, s2) {
-        let s1Arr = s1.split('')
-        let s2Arr = s2.split('')
-        let commonLetters = 0
-        s1Arr.map(s1Letter => {
-            s2Arr.map(s2Letter => {
-                if(s1Letter === s2Letter){
-                    s1Arr[s1Arr.indexOf(s1Letter)] = 0
-                    s2Arr[s2Arr.indexOf(s2Letter)] = 0
-                }
-            })
-        })
-        s1Arr.map(letter => {
-            if(letter === 0){
-                commonLetters++
-            }
-        })
-        return commonLetters
+  const s1Arr = s1.split('');
+  const s2Arr = s2.split('');
+  let commonLetters = 0;
+  s1Arr.map((s1Letter) => {
+    s2Arr.map((s2Letter) => {
+      if (s1Letter === s2Letter) {
+        s1Arr[s1Arr.indexOf(s1Letter)] = 0;
+        s2Arr[s2Arr.indexOf(s2Letter)] = 0;
+      }
+      return s2Letter;
+    });
+    return s1Letter;
+  });
+  s1Arr.map((letter) => {
+    if (letter === 0) {
+      commonLetters++;
     }
+    return letter;
+  });
+  return commonLetters;
+}
 
 module.exports = getCommonCharacterCount;
