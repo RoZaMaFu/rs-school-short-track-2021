569db1b09ccf9bc72597df14f9441aed6f92db06
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 20:47:28 +0300

    upd: eslint fixes

--- src/13-stack.js
+++ src/13-stack.js
@@ -1,19 +1,20 @@
 class Stack {
-    constructor(){
-        this.stack = []
-    }
-    push(elem) {
-      this.stack.push(elem)
-      return 1 
-    }
-  
-    pop() {
-      return this.stack.pop()
-    }
-  
-    peek() {
-      return this.stack[0]
-    }
+  constructor() {
+    this.stack = [];
   }
+
+  push(elem) {
+    this.stack.push(elem);
+    return 1;
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[0];
+  }
+}
 
 module.exports = Stack;
