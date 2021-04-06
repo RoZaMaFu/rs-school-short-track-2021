7ee0ef3296e4a8241fd9b7f856ce91d87899a2c3
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 19:40:30 +0300

    fix: remove spaces

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,26 +1,3 @@
-/**
- * In the popular Minesweeper game you have a board with some mines and those cells
- * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
- * we want to create a Minesweeper game setup.
- *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
- *
- * @example
- * matrix = [
- *  [true, false, false],
- *  [false, true, false],
- *  [false, false, false]
- * ]
- *
- * The result should be following:
- * [
- *  [1, 2, 1],
- *  [2, 1, 1],
- *  [1, 1, 1]
- * ]
- */
 function minesweeper(matr) {
     let resultMatr = []
     matr.map(line => {
@@ -46,5 +23,4 @@ function minesweeper(matr) {
     })
     return resultMatr.map(line => line.filter(field => field !== '-1' && field !== NaN))
 }
-
 module.exports = minesweeper;
