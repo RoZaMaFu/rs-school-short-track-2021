756ebd9ba40d82ba84a171a0268fda65bbf739e6
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 20:42:45 +0300

    upd: eslint fixes

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,26 +1,30 @@
 function minesweeper(matr) {
-    let resultMatr = []
-    matr.map(line => {
-        let tempLine = []
-        line.map(field => {
-            tempLine.push(0)
-        })
-        resultMatr.push(tempLine)
-    })
-    matr.map((line, i) => {
-        line.map((field, j) => {
-            if(field){
-                try { resultMatr[i][j+1]++ } catch(e){ }
-                try { resultMatr[i][j-1]++  } catch(e){ }
-                try { resultMatr[i+1][j+1]++ } catch(e){ } 
-                try { resultMatr[i+1][j]++ } catch(e){ } 
-                try { resultMatr[i+1][j-1]++ } catch(e){ } 
-                try { resultMatr[i-1][j+1]++ } catch(e){ } 
-                try { resultMatr[i-1][j]++ } catch(e){ } 
-                try { resultMatr[i-1][j-1]++  } catch(e){ }   
-            }
-        })
-    })
-    return resultMatr.map(line => line.filter(field => field !== '-1' && field !== NaN))
+  const resultMatr = [];
+  matr.map((line) => {
+    const tempLine = [];
+    line.map((field) => {
+      tempLine.push(0);
+      return field;
+    });
+    resultMatr.push(tempLine);
+    return line;
+  });
+  matr.map((line, i) => {
+    line.map((field, j) => {
+      if (field) {
+        try { resultMatr[i][j + 1]++; } catch (e) { console.log(e); }
+        try { resultMatr[i][j - 1]++; } catch (e) { console.log(e); }
+        try { resultMatr[i + 1][j + 1]++; } catch (e) { console.log(e); }
+        try { resultMatr[i + 1][j]++; } catch (e) { console.log(e); }
+        try { resultMatr[i + 1][j - 1]++; } catch (e) { console.log(e); }
+        try { resultMatr[i - 1][j + 1]++; } catch (e) { console.log(e); }
+        try { resultMatr[i - 1][j]++; } catch (e) { console.log(e); }
+        try { resultMatr[i - 1][j - 1]++; } catch (e) { console.log(e); }
+      }
+      return field;
+    });
+    return line;
+  });
+  return resultMatr.map((line) => line.filter((field) => field !== '-1' && !Number.isNaN(field)));
 }
 module.exports = minesweeper;
