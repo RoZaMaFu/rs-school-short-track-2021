19d05a9daa2a46d8874130c8d609d233fb7f1c18
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 19:43:14 +0300

    fix: remove spaces

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -1,16 +1,3 @@
-/**
- * Given a sorted array, find the index of the element with the given value.
- * Time complexity should be O(logN)
- *
- * @param {Array} array
- * @param {Number} value
- * @return {Number}
- *
- * @example
- * For ([1, 2, 3], 1) should return 0
- * For ([1, 2, 3], 2) should return 1
- *
- */
 function findIndex(arr, val) {
     return arr.indexOf(val)
   }
