3b8a84fd59cf4816097d45e54196000513ad1a6d
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 10:07:04 +0300

    feat: add delete digit func

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,17 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
-}
+function deleteDigit(n) {
+        let maxNum = 0
+        let numArr = n.toString().split('')
+        numArr.map((num, i) => {
+            let tempArr = numArr
+            tempArr.splice(i, 1)
+            if(Number(tempArr.join('')) > maxNum){
+                maxNum = Number(tempArr.join(''))
+            }
+        })
+        return maxNum
+    }
 
 module.exports = deleteDigit;
