43adff94cb4325abb9b25a74161b8d60e74fcdfd
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 20:44:51 +0300

    upd: eslint fixes

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -1,10 +1,11 @@
 function removeKFromList(l, k) {
-    l.map((int, i) => {
-        if(int  === k ){
-            l.splice(i, 1)
-        }
-    })
-    return l
-  }
+  l.map((int, i) => {
+    if (int === k) {
+      l.splice(i, 1);
+    }
+    return int;
+  });
+  return l;
+}
 
 module.exports = removeKFromList;
