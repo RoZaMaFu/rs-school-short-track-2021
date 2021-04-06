65dd9e2e8211c9f308076e958464aab58e88057a
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 20:43:15 +0300

    upd: eslint fixes

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -1,14 +1,16 @@
 function deleteDigit(n) {
-        let maxNum = 0
-        let numArr = n.toString().split('')
-        numArr.map((num, i) => {
-            let tempArr = numArr
-            tempArr.splice(i, 1)
-            if(Number(tempArr.join('')) > maxNum){
-                maxNum = Number(tempArr.join(''))
-            }
-        })
-        return maxNum
+  let maxNum = 0;
+  const numArr = n.toString().split('');
+  numArr.map((num, i) => {
+    const tempArr = numArr;
+    tempArr.splice(i, 1);
+    if (Number(tempArr.join('')) > maxNum) {
+      maxNum = Number(tempArr.join(''));
     }
+    return num;
+  });
+  return maxNum;
+}
 
 module.exports = deleteDigit;
+
