e347be80483f1b06263eab0800e96cae12e5660a
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 19:05:19 +0300

    feat: add minesweeper func

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,30 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matr) {
+    let resultMatr = []
+    matr.map(line => {
+        let tempLine = []
+        line.map(field => {
+            tempLine.push(0)
+        })
+        resultMatr.push(tempLine)
+    })
+    matr.map((line, i) => {
+        line.map((field, j) => {
+            if(field){
+                try { resultMatr[i][j+1]++ } catch(e){ }
+                try { resultMatr[i][j-1]++  } catch(e){ }
+                try { resultMatr[i+1][j+1]++ } catch(e){ } 
+                try { resultMatr[i+1][j]++ } catch(e){ } 
+                try { resultMatr[i+1][j-1]++ } catch(e){ } 
+                try { resultMatr[i-1][j+1]++ } catch(e){ } 
+                try { resultMatr[i-1][j]++ } catch(e){ } 
+                try { resultMatr[i-1][j-1]++  } catch(e){ }   
+            }
+        })
+    })
+    return resultMatr.map(line => line.filter(field => field !== '-1' && field !== NaN))
 }
 
 module.exports = minesweeper;
