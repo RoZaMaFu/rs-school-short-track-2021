a90f53fb076e4fef639aca9f9fb83522cadfeb50
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 19:43:34 +0300

    fix: remove spaces

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -1,13 +1,3 @@
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
 function encodeLine(str) {
     let strArr = str.split('')
     let resultArr = []
