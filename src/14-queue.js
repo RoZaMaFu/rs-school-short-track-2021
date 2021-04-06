46897129a5960cf5902407bd6afc331a5c176181
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 20:47:52 +0300

    upd: eslint fixes

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,18 +1,19 @@
 class Queue {
-    constructor(){
-        this.queue = []
-    }
-    get size() {
-      return this.queue.length
-    }
-  
-    enqueue(elem) {
-      this.queue.push(elem)
-    }
-  
-    dequeue() {
-      return this.queue.shift()
-    }
+  constructor() {
+    this.queue = [];
   }
+
+  get size() {
+    return this.queue.length;
+  }
+
+  enqueue(elem) {
+    this.queue.push(elem);
+  }
+
+  dequeue() {
+    return this.queue.shift();
+  }
+}
 
 module.exports = Queue;
