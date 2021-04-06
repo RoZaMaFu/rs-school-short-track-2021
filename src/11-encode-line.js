91710510c0957e5d5c3b0d4c42af11c78abb4016
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 14:51:16 +0300

    feat: add encode func

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+    let strArr = str.split('')
+    let resultArr = []
+    for(let i = 0; i < strArr.length; i++){
+        let counter = 1
+        for(let j = i+1; j < strArr.length; j++){
+            if(strArr[i] === strArr[j] && strArr[j]){
+                counter++
+                strArr[j] = null
+            }
+        }
+        if(strArr[i]){
+             resultArr.push(counter === 1 ? `${strArr[i]}` : `${counter}${strArr[i]}`)
+        }
+    }
+    return resultArr.join('')
 }
 
 module.exports = encodeLine;
