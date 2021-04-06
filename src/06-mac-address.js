4ec3ff702ed2aad43aa733b1f7142f6c03e8cf19
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-06 20:43:43 +0300

    upd: eslint fixes

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -1,9 +1,9 @@
 function isMAC48Address(mac) {
-    if(mac.split('-').join('').length !== 12){
-        return false
-    }
-    let regex = /[^A-Z0-9]/g
-    return !regex.test(mac.split('-').join(''))
+  if (mac.split('-').join('').length !== 12) {
+    return false;
   }
+  const regex = /[^A-Z0-9]/g;
+  return !regex.test(mac.split('-').join(''));
+}
 
 module.exports = isMAC48Address;
