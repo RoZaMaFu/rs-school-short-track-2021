9c9359efa0cd521cf03860d77881ce4be211f387
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-07 19:21:53 +0300

    feat: add func

--- src/13-stack.js
+++ src/13-stack.js
@@ -1,1 +1,21 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(elem) {
+    this.stack.push(elem);
+    return 1;
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+}
+
+module.exports = Stack;
 
