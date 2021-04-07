f6fcd7a07270a8b06799fb01a2d2cc0f0733e256
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-07 17:39:17 +0300

    fix: eslint errors fix

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -1,5 +1,13 @@
 function sortByHeight(arr) {
-  const tempArr = arr.filter((numb) => numb !== -1).sort();
+  const tempArr = arr.filter((numb) => numb !== -1).sort((a, b) => {
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    }
+    return 0;
+  });
   arr.map((int, i) => {
     if (int === -1) {
       tempArr.splice(i, 0, -1);
@@ -10,4 +18,5 @@ function sortByHeight(arr) {
 }
 
 module.exports = sortByHeight;
+
 
