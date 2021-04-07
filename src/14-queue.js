e1b16ecb667467a082aa1fff53c31e0796f10359
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-07 17:44:47 +0300

    remove: remove func

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,19 +1,1 @@
-class Queue {
-  constructor() {
-    this.queue = [];
-  }
 
-  get size() {
-    return this.queue.length;
-  }
-
-  enqueue(elem) {
-    this.queue.push(elem);
-  }
-
-  dequeue() {
-    return this.queue.shift();
-  }
-}
-
-module.exports = Queue;
