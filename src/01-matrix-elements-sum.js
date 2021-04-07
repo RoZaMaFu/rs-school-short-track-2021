e2ca5ce9c984399fdf874b88c2017f067e430dbd
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-07 19:20:08 +0300

    feat: add func

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,1 +1,19 @@
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  const tempMatrix = matrix;
+  tempMatrix.map((line, i) => {
+    line.map((numb, j) => {
+      if (numb === 0) {
+        if (i < tempMatrix.length - 1) {
+          tempMatrix[i + 1][j] = 0;
+        }
+      }
+      sum += numb;
+      return numb;
+    });
+    return line;
+  });
+  return sum;
+}
 
+module.exports = getMatrixElementsSum;
