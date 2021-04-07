504849bc5a4d1c13e938ea8098fd46533ad63d6b
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-07 19:47:55 +0300

    fix: add func

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -1,5 +1,12 @@
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let index = -1;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      index = i;
+      break;
+    }
+  }
+  return index;
 }
 
 module.exports = findIndex;
