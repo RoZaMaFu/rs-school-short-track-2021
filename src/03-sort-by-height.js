ea264ce3d49ad6f7f56c7dd5237931793ccd34c7
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-04-07 19:23:33 +0300

    feat: add func

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -1,1 +1,20 @@
+function sortByHeight(arr) {
+  const tempArr = arr.filter((numb) => numb !== -1).sort((a, b) => {
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    }
+    return 0;
+  });
+  arr.map((int, i) => {
+    if (int === -1) {
+      tempArr.splice(i, 0, -1);
+    }
+    return int;
+  });
+  return tempArr;
+}
 
+module.exports = sortByHeight;
